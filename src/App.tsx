02182f4dadcd183e43628b0f1dc49a9e7bf3a4b6
Cole Lawrence <cole@colelawrence.com>
2022-02-28 15:07:06 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+
+import "./App.css";
+
+import { BlockComponent } from "blockprotocol/react";
+import { editableEntity } from "./editableEntity";
+
+type AppProps = {
+  productTitle: string;
+  productDescription: string;
+  productImageURL: string;
+  productLinkURL: string; // URL
+};
+
+export const App: BlockComponent<AppProps> = (props) => {
+  const useEditable = editableEntity(props);
+  const productTitle = useEditable("productTitle", (view) => (
+    <h3 className="empty-placeholder" {...view.clickToEdit}>
+      {view.isEditing ? view.editingUI : view.value}
+    </h3>
+  ));
+  const productImage = useEditable("productImageURL", (view) => (
+    <>
+      <img
+        {...view.clickToEdit}
+        src={view.value}
+        style={{ width: "auto", minWidth: "4rem", maxWidth: "10rem", height: "auto", minHeight: "4rem", maxHeight: "10rem" }}
+      />
+      {view.isEditing && (
+        // absolute to hover below
+        <div style={{ position: "absolute", background: "white", padding: "1rem" }}>
+          {view.editingUI}
+        </div>
+      )}
+    </>
+  ));
+  const productDescription = useEditable("productDescription", (view) => (
+    <p
+      {...view.clickToEdit}
+      className="empty-placeholder"
+      style={{ fontSize: "0.8rem", opacity: "0.8" }}
+    >
+      {view.isEditing ? view.editingUI : view.value}
+    </p>
+  ));
+
+  const productLinkTile = useEditable("productLinkURL", (view) => {
+    return (
+      <a
+        href={view.value}
+        className="simple-product-link"
+        style={{ display: "block", textDecoration: "none" }}
+        onContextMenu={(evt) => {
+          // right click
+          view.edit();
+        }}
+      >
+        {productTitle}
+        {productImage}
+        {productDescription}
+        {view.isEditing ? (
+          <div>Edit URL: {view.editingUI}</div>
+        ) : view.isEditable ? (
+          <button {...view.clickToEdit}>Edit URL</button>
+        ) : null}
+      </a>
+    );
+  });
+
+  return productLinkTile;
+};
---
+++ src/editableEntity.tsx
@@ -0,0 +1,122 @@
+import * as React from "react";
+
+import { BlockProtocolProps } from "blockprotocol";
+
+type EditableOptions<T> = {
+  value: T;
+  isEditable: boolean;
+  edit(): void;
+  clickToEdit: { onClick: React.MouseEventHandler };
+} & (
+  | {
+      isEditing: false;
+    }
+  | {
+      isEditing: true;
+      editingUI: JSX.Element;
+    }
+);
+
+export function editableEntity<BlockProps>(
+  initialEntity: BlockProps & BlockProtocolProps
+): <P extends keyof BlockProps>(
+  key: P,
+  renderFn: (view: EditableOptions<BlockProps[P]>) => JSX.Element
+  // editFn: (editing: { ui: JSX.Element | null }) => JSX.Element
+) => JSX.Element {
+  return function useEditableProp(key, renderFn) {
+    const [editing, setEditing] = React.useState(false);
+    const [saveValue, setSaveValue] = React.useState(initialEntity[key]);
+    const [value, setValue] = React.useState(saveValue);
+
+    return renderFn({
+      value,
+      edit() {
+        if (!editing) {
+          setEditing(true);
+        }
+      },
+      clickToEdit: {
+        onClick() {
+          if (!editing) {
+            setEditing(true);
+          }
+        },
+      },
+      isEditing: editing,
+      isEditable: true, // TODO: wire up based on protocol writability
+      editingUI: editing ? (
+        <div className="editor-value">
+          <EditingUI
+            label={key as string}
+            value={value}
+            setValue={setValue}
+            dismiss={() => {
+              setEditing(false);
+              setValue(saveValue);
+            }}
+            saveValue={() => {
+              setSaveValue(value);
+              setEditing(false);
+            }}
+          />
+        </div>
+      ) : undefined,
+    });
+  };
+}
+
+function EditingUI<T>(props: {
+  label?: string;
+  value: T;
+  setValue(updated: T): void;
+  saveValue(): void;
+  dismiss(): void;
+}) {
+  return (
+    <label className="editing-ui">
+      {/* {props.label && <div className="editing--label">{props.label}</div>} */}
+      <input
+        style={{
+          font: "inherit",
+          color: "inherit",
+          background: "transparent",
+          border: "none",
+        }}
+        value={props.value as any}
+        autoFocus
+        onInput={function (evt) {
+          props.setValue(evt.target["value"]);
+        }}
+        onKeyDown={(evt) => {
+          if (evt.key === "Enter") {
+            props.saveValue();
+          } else if (evt.key === "Escape") {
+            props.dismiss();
+          }
+        }}
+      />
+      <div className="editing-ui--controls">
+        <button {...clickOrEnter(props.dismiss)}>× Dismiss</button>
+        <button {...clickOrEnter(props.saveValue)}>✓ Save</button>
+      </div>
+    </label>
+  );
+}
+
+function clickOrEnter(then: () => void) {
+  return {
+    onClick(evt: React.MouseEvent) {
+      if (evt.button === 0) {
+        evt.stopPropagation();
+        then();
+      }
+    },
+    onKeydown(evt: React.KeyboardEvent) {
+      if (evt.key === "Enter") {
+        evt.stopPropagation();
+        then();
+      }
+    },
+  };
+}
